3a8c91d6a0f1b4c462f49bfa4f84c8e398b2eab1
wjforero <1499698+wjforero@users.noreply.github.com>
2024-08-30 19:28:58 -0500

    Initial commit

---
+++ app/src/main/java/com/example/turtlesapp/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.turtlesapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.turtlesapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    var binding:ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding?.btnRaphael?.setOnClickListener {
+            showCharacter("raphael")
+        }
+
+        binding?.btnDonatello?.setOnClickListener {
+            showCharacter("donatello")
+        }
+
+        binding?.btnMike?.setOnClickListener {
+            showCharacter("michelangelo")
+        }
+    }
+
+    fun showCharacter(name: String) {
+        if(name=="raphael")
+            binding?.imgCharacter?.setImageResource(R.drawable.raphael)
+        if(name=="donatello")
+            binding?.imgCharacter?.setImageResource(R.drawable.donatello)
+        if(name=="michelangelo")
+            binding?.imgCharacter?.setImageResource(R.drawable.michelangelo)
+    }
+}
